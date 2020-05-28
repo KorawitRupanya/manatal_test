f78f13a5c2728013234ad2a3002d84b69e93418e
Korwit <korawit.r@ku.th>
2020-05-28 15:21:21 +0700

    Change some layout of NewsCard

--- src/components/NewsCard.vue
+++ src/components/NewsCard.vue
@@ -1,17 +1,11 @@
 <template>
-  <v-layout column>
-    <v-flex xs10 sm6 offset-sm3>
+  <v-layout row>
+    <v-flex>
       <div v-for="article in articles" :key="article.title">
         <v-card class="my-3" hover>
           <v-container fluid grid-list-md>
             <v-layout row wrap>
-              <v-img height="350px" v-bind:src="article.urlToImage" />
-              <v-flex
-                      v-for="card in cards"
-                      v-bind="{ [`xs${card.flex}`]: true }"
-                      :key="card.title"
-              >
-              </v-flex>
+              <v-img height="350px"  v-bind:src="article.urlToImage" />
               <v-container fill-height fluid>
                 <v-layout>
                   <v-flex xs10 align-end d-flex>
@@ -20,8 +14,8 @@
                     </v-chip>
                     <v-spacer/>
                     <span class="headline">{{ article.title }}
+                      <ChangeHeaderDialog/>
                     </span>
-                    <ChangeHeaderDialog/>
                   </v-flex>
                 </v-layout>
               </v-container>
@@ -58,10 +52,5 @@
     props: {
       articles: Array
     },
-    data(){
-      return{
-        cards: []
-      }
-    }
   };
 </script>
